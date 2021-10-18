03e86ee6967c6da57a736d0f22baff2f354004fe
kabu1204 <201930344367@mail.scut.edu.cn>
2021-10-18 09:19:20 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/vfs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vfs.dir/main.cpp.o"
+  "vfs"
+  "vfs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
